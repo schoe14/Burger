35e6f2d6b9eaf514902eb3251a9319db1dfbaa69
Seohui Choe <chelsea.choe@gmail.com>
2020-02-10 17:13:05 -0500

    add initial structure

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger',false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chili Burger',true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Luther Burger',false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Burger',true);
---
+++ public/assets/js/index.js
@@ -0,0 +1,59 @@
+$(function () {
+    $(".change-devour").on("click", function (event) {
+        const id = $(this).data("id");
+        const newDevour = $(this).data("newdevour");
+
+        const newDevourState = {
+            devoured: newDevour
+        };
+
+        console.log("new devourState: " + newDevourState); // test
+
+        // Send the PUT request.
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newDevourState
+        }).then(
+            function () {
+                console.log("changed devour to", newDevour); // test
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+    $(".delete-burger").on("click", function (event) {
+        const id = $(this).data("id");
+
+        // Send the DELETE request.
+        $.ajax("/api/burgers/" + id, {
+            type: "DELETE",
+        }).then(
+            function () {
+                console.log("deleted: ", id);
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+    $(".create-form").on("submit", function (event) {
+        // Make sure to preventDefault on a submit event.
+        event.preventDefault();
+
+        const newHamburger = {
+            burger_name: $("#burger_name").val().trim(),
+            devoured: $("[name=devoured]:checked").val().trim()
+        };
+
+        // Send the POST request.
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newHamburger
+        }).then(
+            function () {
+                console.log("created new hamburger"); // test
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+});
